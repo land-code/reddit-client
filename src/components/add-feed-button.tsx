660fcc1403f72e0c7764a2ad85cd1d0d3d123eb7
David García Díaz <davidgrcdiaz@gmail.com>
2024-09-10 11:27:14 +0200

    Add input to dialog

--- src/components/add-feed-button.tsx
+++ src/components/add-feed-button.tsx
@@ -1,5 +1,7 @@
 import { Button } from "./ui/button";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
 
 export default function AddFeedButton () {
   return (
@@ -11,9 +13,21 @@ export default function AddFeedButton () {
         <DialogHeader>
           <DialogTitle>Add feed</DialogTitle>
           <DialogDescription>
-            Add a new feed to your dashboard by entering the subreddit name below. For example, &quotr/programming&quot or &quotr/technology&quot.
+            Add a new feed to your dashboard by entering the subreddit name below. For example, &quot;r/programming&quot; or &quot;r/technology&quot;.
           </DialogDescription>
         </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input
+              id="name"
+              defaultValue="r/programming"
+              className="col-span-3"
+            />
+          </div>
+        </div>
         <DialogFooter>
           <Button type="submit">Add feed</Button>
         </DialogFooter>
