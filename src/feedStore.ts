91eccfce2a5bb35c2527ae469ada1862643ff57c
David García Díaz <davidgrcdiaz@gmail.com>
2024-09-10 17:05:35 +0200

    Add feed store to local storage

--- src/feedStore.ts
+++ src/feedStore.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand'
 import { Feed } from './types/Feed'
+import { persist } from 'zustand/middleware'
 
 type FeedStore = {
   feeds: Feed[]
@@ -7,16 +8,21 @@ type FeedStore = {
   removeFeed: (feedName: string) => void
 }
 
-export const useFeedStore = create<FeedStore>(set => ({
-  feeds: [],
-  addFeed: feed =>
-    set(state => {
-      if (state.feeds.find(f => f.name === feed.name))
-        throw new Error('Feed already exists')
-      return { feeds: [...state.feeds, feed] }
+export const useFeedStore = create<FeedStore>()(
+  persist(
+    set => ({
+      feeds: [],
+      addFeed: feed =>
+        set(state => {
+          if (state.feeds.find(f => f.name === feed.name))
+            throw new Error('Feed already exists')
+          return { feeds: [...state.feeds, feed] }
+        }),
+      removeFeed: feedName =>
+        set(state => ({
+          feeds: state.feeds.filter(f => f.name !== feedName)
+        }))
     }),
-  removeFeed: feedName =>
-    set(state => ({
-      feeds: state.feeds.filter(f => f.name !== feedName)
-    }))
-}))
+    { name: 'feed-store' }
+  )
+)
